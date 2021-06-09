4180fb68fd61b2aafd92866259e7f923b4b90df3
root <root@localhost.localdomain>
2021-06-08 21:39:48 -0500

    ready for personal server

--- index.js
+++ index.js
@@ -3,44 +3,86 @@ const axios = require("axios");
 const redis = require("redis");
 
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 
 //redis
-const redisPort = 6379
-//localhost
-//const client = redis.createClient(redisPort);
-//class
-const client = redis.createClient({host: '35.232.232.192', port: redisPort});
-
+const redisPort = 6379;
+const client = redis.createClient(redisPort);
 client.on("error", (err) => {
 	console.log(err);
 })
 
-app.get("/jobs", async (req, res) => {
-	const searchTerm = req.query.search;
-	console.log(searchTerm);
-	try {
-        client.get(searchTerm, async (err, jobs) => {
-            if (err) throw err;
-    
-            if (jobs) {
-                res.status(200).send({
-                    jobs: JSON.parse(jobs),
-                    message: "data retrieved from the cache"
-                });
-            }
-            else {
-                const jobs = await axios.get(`https://jobs.github.com/positions.json?search=${searchTerm}`);
-                client.setex(searchTerm, 600, JSON.stringify(jobs.data));
-                res.status(200).send({
-                    jobs: jobs.data,
-                    message: "cache miss"
-                });
-            }
-        });
-    } catch(err) {
-        res.status(500).send({message: err.message});
-    }
+//endredis
+
+app.get("/emails", async(req,res) => {
+	try{
+		client.get(1,async(err,people) => {
+			if(err) throw err;
+
+			if(people){
+				res.status(200).send({
+					people: JSON.parse(people),
+					message: "data retrieven from the REDIS cache"
+				});
+			}
+			else {
+				const people = await axios.get(`http://35.223.238.250:8145/api/auth/emails`);
+				client.setex(JSON.stringify(people.data));
+				res.status(200).send({
+					people: people.data,
+					message: "cache miss, data from REST-API"
+				});
+			}
+
+		});
+	} catch (err) {
+		rest.status(500).send({message: err.message});
+	}
 });
+
+app.listen(process.env.PORT || 8085, () => {
+	console.log("Node server started");
+});
+
+//redis
+//const redisPort = 6379
+//localhost
+//const client = redis.createClient(redisPort);
+//class
+//const client = redis.createClient({host: '35.232.232.192', port: redisPort});
+
+//client.on("error", (err) => {
+//	console.log(err);
+//})
+
+//app.get("/jobs", async (req, res) => {
+//	const searchTerm = req.query.search;
+//	console.log(searchTerm);
+//	try {
+     //   client.get(searchTerm, async (err, jobs) => {
+   //         if (err) throw err;
+    
+            //if (jobs) {
+            //    res.status(200).send({
+          //          jobs: JSON.parse(jobs),
+        //            message: "data retrieved from the cache"
+      //          });
+    //        }
+  //          else {
+//                const jobs = await axios.get(`https://jobs.github.com/positions.json?search=${searchTerm}`);
+                //client.setex(searchTerm, 600, JSON.stringify(jobs.data));
+               // res.status(200).send({
+              //      jobs: jobs.data,
+            //        message: "cache miss"
+          //      });
+        //    }
+      //  });
+    //} catch(err) {
+    //    res.status(500).send({message: err.message});
+  //  }
+//});
 
 //local
 //app.listen(process.env.PORT || 8085, () => {
@@ -48,6 +90,6 @@ app.get("/jobs", async (req, res) => {
 //});
 
 //class
-app.listen(process.env.PORT || 8162, () => {
-	console.log("Node server started: 8085");
-});
+//app.listen(process.env.PORT || 8162, () => {
+	//console.log("Node server started: 8085");
+//});
